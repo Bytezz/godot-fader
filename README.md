2966c46d7459eef2443ee55c520f972bf9a7acaf
Bytez <25850701+Bytezz@users.noreply.github.com>
2022-01-18 16:51:55 +0100

    Firs commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fader
+
+Hide/Show any node with a fade animation.
+
+# Usage
+
+`Fader.fade_in(Node, Seconds)`
+
+or
+
+`Fader.fade_out(Node, Seconds)`
+
+# LICENSE
+
+[GPLv3](LICENSE)
---
+++ addons/Fader/fader.gd
@@ -0,0 +1,34 @@
+## Coded by Bytez
+## https://github.com/bytezz/godot-fader
+## ----
+## Hide/Show any node with a fade animation
+## (for Godot Game Engine).
+
+extends Node
+class_name Fader
+
+const _fadeAnimatorScene = preload("fadeAnimator.tscn")
+
+static func fade_in(node:Node, seconds:float=1.0):
+	fade(node, seconds, true)
+
+static func fade_out(node:Node, seconds:float=1.0):
+	fade(node, seconds, false)
+
+static func fade(node:Node, seconds:float=1.0, fadeIn:bool=true) -> void:
+	var fadeAnimator = _fadeAnimatorScene.instance()
+	yield(Engine.get_main_loop().root, "ready")
+	Engine.get_main_loop().root.add_child(fadeAnimator)
+	fadeAnimator.root_node = Engine.get_main_loop().root.get_path()
+	fadeAnimator.get_animation("Fade").track_set_path(0, str(node.get_path()) + ":modulate:a")
+	fadeAnimator.get_animation("Fade").track_set_path(1, str(node.get_path()) + ":visible")
+	
+	fadeAnimator.playback_speed = 1/seconds
+	
+	if fadeIn:
+		fadeAnimator.play("Fade")
+	else:
+		fadeAnimator.play_backwards("Fade")
+	
+	yield(fadeAnimator, "animation_finished")
+	fadeAnimator.queue_free()
