4b4252a4b5eb62e755627c6600c7196174658752
Bytez <25850701+Bytezz@users.noreply.github.com>
2022-01-18 17:31:40 +0100

    Fixed error if node is already ready.

--- addons/Fader/fader.gd
+++ addons/Fader/fader.gd
@@ -9,15 +9,17 @@ class_name Fader
 
 const _fadeAnimatorScene = preload("fadeAnimator.tscn")
 
-static func fade_in(node:Node, seconds:float=1.0):
+static func fade_in(node:Node, seconds:float=1.0) -> void:
 	fade(node, seconds, true)
 
-static func fade_out(node:Node, seconds:float=1.0):
+static func fade_out(node:Node, seconds:float=1.0) -> void:
+	print(0)
 	fade(node, seconds, false)
 
 static func fade(node:Node, seconds:float=1.0, fadeIn:bool=true) -> void:
 	var fadeAnimator = _fadeAnimatorScene.instance()
-	yield(Engine.get_main_loop().root, "ready")
+	if not Engine.get_main_loop().root.has_node("/root"):
+		yield(Engine.get_main_loop().root, "ready")
 	Engine.get_main_loop().root.add_child(fadeAnimator)
 	fadeAnimator.root_node = Engine.get_main_loop().root.get_path()
 	fadeAnimator.get_animation("Fade").track_set_path(0, str(node.get_path()) + ":modulate:a")
@@ -30,5 +32,5 @@ static func fade(node:Node, seconds:float=1.0, fadeIn:bool=true) -> void:
 	else:
 		fadeAnimator.play_backwards("Fade")
 	
-	yield(fadeAnimator, "animation_finished")
-	fadeAnimator.queue_free()
+	#yield(fadeAnimator, "animation_finished")
+	#fadeAnimator.queue_free()
